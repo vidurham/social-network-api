4ee7daf0e04a337ef761d63ab49f5b52d1f597f2
vidurham <vdurham@prideseals.com>
2022-06-23 17:40:57 -0400

    fixed up models for social media backend

--- models/Thought.js
+++ models/Thought.js
@@ -1,21 +1,21 @@
 const { Schema, model, Types } = require('mongoose');
 const dateFormat = require('../utils/dateFormat');
 
-const ReplySchema = new Schema(
+const ReactionSchema = new Schema(
   {
-    // set custom id to avoid confusion with parent comment _id
-    replyId: {
+    reactionId: {
       type: Schema.Types.ObjectId,
       default: () => new Types.ObjectId()
     },
-    replyBody: {
-      type: String,
-      required: true
-    },
-    writtenBy: {
+    reactionBody: {
       type: String,
       required: true,
-      trim: true
+      minLength: 1,
+      maxLength: 280
+    },
+    username: {
+      type: String,
+      required: true
     },
     createdAt: {
       type: Date,
@@ -26,27 +26,29 @@ const ReplySchema = new Schema(
   {
     toJSON: {
       getters: true
-    }
+    },
+    id: false
   }
 );
 
-const CommentSchema = new Schema(
+const ThoughtSchema = new Schema(
   {
-    writtenBy: {
+    thoughtText: {
       type: String,
-      required: true
-    },
-    commentBody: {
-      type: String,
-      required: true
+      required: true,
+      minLength: 1,
+      maxLength: 280
     },
     createdAt: {
       type: Date,
       default: Date.now,
       get: createdAtVal => dateFormat(createdAtVal)
     },
-    // use ReplySchema to validate data for a reply
-    replies: [ReplySchema]
+    username: {
+      type: String,
+      required: true
+    },
+    reactions: [ReactionSchema]
   },
   {
     toJSON: {
@@ -57,10 +59,10 @@ const CommentSchema = new Schema(
   }
 );
 
-CommentSchema.virtual('replyCount').get(function() {
-  return this.replies.length;
+ThoughtSchema.virtual('reactionCount').get(function() {
+  return this.reactions.length;
 });
 
-const Comment = model('Comment', CommentSchema);
+const Thought = model('Thought', ThoughtSchema);
 
-module.exports = Comment;
+module.exports = Thought;
