cc90470bbb2086074e8811129dd40eb6a8054fe1
vidurham <vdurham@prideseals.com>
2022-06-23 18:29:58 -0400

    add friends and delete friends added to user routes

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -66,7 +66,42 @@ const userController = {
         res.json(dbUserData);
       })
       .catch(err => res.json(err));
-  }
+  },
+
+  // add friend
+  addFriend({ params }, res) {
+    User.findOneAndUpdate(
+      { _id: params.userId},
+      { $addToSet: { friends: params.friendId }},
+      { new: true }
+    )
+    .then(dbUserData => {
+      if (!dbUserData) {
+        res.status(404).json({ message: 'No user found with this id!' });
+          return;
+        }
+      res.json(dbUserData);
+    })
+    .catch(err => res.json(err));
+  },
+
+  // delete friend
+  deleteFriend({ params }, res) {
+    User.findOneAndUpdate(
+      { _id: params.userId},
+      { $pull: { friends: params.friendId }},
+      { new: true }
+    )
+    .then(dbUserData => {
+      if (!dbUserData) {
+        res.status(404).json({ message: 'No user found with this id!' });
+        return;
+      }
+      res.json(dbUserData);
+    })
+    .catch(err => res.json(err));
+  },
 };
+
 
 module.exports = userController;
